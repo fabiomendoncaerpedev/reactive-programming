3b763dc87c1812978c545e396b4f014e0decd66c
Fabio Mendonça <fabio.mendonca@totvs.com.br>
2024-09-21 08:00:03 -0300

    feat: rendering of search results

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,15 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { LivroService } from './../../services/livro.service';
+import { Item, Livro } from 'src/app/models/interfaces';
 
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent {
+export class ListaLivrosComponent implements OnDestroy {
 
-  listaLivros: [];
+  listaLivros: Array<Livro>;
+  campoBusca = '';
+  subscription: Subscription;
+  livro: Livro;
 
-  constructor() { }
+  constructor(
+    private livroService: LivroService
+  ) { }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  buscarLivros() {
+    this.subscription = this.livroService.buscar(this.campoBusca).subscribe({
+      next: (items) => {
+        this.listaLivros = this.parseLivrosResultadosParaLivros(items);
+      },
+      error: (error) => console.log(error)
+    });
+  }
+
+  parseLivrosResultadosParaLivros(items: Array<Item>): Array<Livro> {
+    const livros: Array<Livro> = [];
+
+    items.forEach(item => {
+      livros.push(this.livro = {
+        title: item.volumeInfo?.title,
+        authors: item.volumeInfo?.authors,
+        publisher: item.volumeInfo?.publisher,
+        publishedDate: item.volumeInfo?.publishedDate,
+        description: item.volumeInfo?.description,
+        previewLink: item.volumeInfo?.previewLink,
+        thumbnail: item.volumeInfo?.imageLinks?.thumbnail
+      })
+    });
+
+    return livros;
+  }
 
 }
 
